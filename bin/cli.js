3406ae6a2e439d92045750e4c778f0aaae7fcdd3
朱奕腾 <zytmyth@qq.com>
2020-08-19 19:39:32 +0800

    fix: cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
-const hasFlag = require('has-flag')
-require('../lib/cli')
-  .run({
-    help: hasFlag('-h'),
-    force: hasFlag('-f'),
-    silent: hasFlag('-s'),
+const { Command } = require('commander')
+const path = require('path')
+const program = new Command()
+
+program
+  .option('-d, --dir <string>', '项目根路径')
+  .option('-i, --ignore <string>', '忽略某个项目，支持正则')
+  .option('-s, --slient', '安静模式')
+  .option('-f, --force', '强制运行')
+  .action(function(cmd, env) {
+    const opts = program.opts()
+    if (opts.dir && !opts.dir.startsWith('/')) {
+      opts.dir = path.resolve(process.cwd(), opts.dir)
+    }
+    require('../lib/cli')
+      .run(opts)
+      .catch(console.error)
   })
-  .catch(console.error)
+
+program.parse(process.argv)
